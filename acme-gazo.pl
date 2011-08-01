8260d2f19b43a44a6aa9c1d084edc8771677159c
Sharl Morlaroll <sharl_github@haun.org>
2011-08-01 10:32:28 +0900

    first commit

---
+++ acme-gazo.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+# -*- Mode: perl -*-
+
+use strict;
+use Getopt::Long;
+use Config::IniHash;
+use Net::IRC;
+use File::Basename;
+use URI;
+use Web::Scraper;
+use Data::Dumper;
+
+my $cfile   = $ENV{HOME} . '/.' . basename($0, '.pl'); # ~/.acme-gazo
+my $channel = undef;
+
+# Use no Carp messages.
+$SIG{__WARN__} = sub {};
+
+my $opt = GetOptions(
+    'file=s'    => \$cfile,
+    'channel=s' => \$channel
+    );
+
+if ($#ARGV != 0 || ! $opt || $ARGV[0] !~ m|^https?://|o) {
+    print STDERR "usage: " . basename($0) . " [--file <config file>] [--channel <channel>] <URL>\n";
+    exit 1;
+}
+my $URI = $ARGV[0];
+my $config = ReadINI($cfile);
+my $channel ||= $config->{defaults}{channel};
+
+my $scr = scraper {
+    process 'a', 'href[]' => sub {
+	grep /jpg$/, $_->{href};
+    };
+    result 'href';
+};
+my $jpgs = $scr->scrape(URI->new($URI));
+
+my $irc = new Net::IRC;
+my $conn = $irc->newconn(
+    Password => $config->{defaults}{password},
+    Nick     => $config->{defaults}{nick},
+    Server   => $config->{defaults}{server},
+    Port     => $config->{defaults}{port}
+    ); 	
+sub on_connect {
+    my $self = shift;
+
+    $self->join($channel);
+    foreach my $jpg (@{$jpgs}) {
+	$self->privmsg($channel, $jpg);
+	sleep(1);
+    }
+    exit;
+}
+$conn->add_handler('endofmotd', \&on_connect);
+$irc->start;
